d0e6377875c2a7d895124e996eea32bf69d8aeb3
Arsene Mkrtchian <arsenmkrtchian17@gmail.com>
2022-02-21 12:17:57 -0500

    tt

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,24 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { RouterModule } from '@angular/router';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BienvenueComponent } from './bienvenue/bienvenue.component';
-import { ProblemeComponent } from './probleme/probleme.component';
-import { AccueilComponent } from './accueil/accueil.component';
+
 
 @NgModule({
   declarations: [
     AppComponent,
-    BienvenueComponent,
-    ProblemeComponent,
-    AccueilComponent
+    BienvenueComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot([
+      { path:'bienvenue', component:BienvenueComponent},
+    {path:'', redirectTo:'bienvenue',pathMatch:'full'}
+    ])
   ],
   providers: [],
   bootstrap: [AppComponent]
