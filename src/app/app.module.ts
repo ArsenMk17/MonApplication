4d4b85808859dbff8585bb26f24ed981780fad05
Arsene Mkrtchian <arsenmkrtchian17@gmail.com>
2022-02-21 12:16:26 -0500

    Bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,15 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BienvenueComponent } from './bienvenue/bienvenue.component';
+import { ProblemeComponent } from './probleme/probleme.component';
+import { AccueilComponent } from './accueil/accueil.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    BienvenueComponent
+    BienvenueComponent,
+    ProblemeComponent,
+    AccueilComponent
   ],
   imports: [
     BrowserModule,
