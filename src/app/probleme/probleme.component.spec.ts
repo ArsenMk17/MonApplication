12b0c7a45eda3116414fa4257453f088f29012dc
Arsene Mkrtchian <arsenmkrtchian17@gmail.com>
2022-02-23 15:10:52 -0500

    Fait zone prenom valide avec 200

--- src/app/probleme/probleme.component.spec.ts
+++ src/app/probleme/probleme.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 
 import { ProblemeComponent } from './probleme.component';
 
@@ -8,22 +9,47 @@ describe('ProblemeComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
+      imports: [ReactiveFormsModule],
       declarations: [ ProblemeComponent ]
     })
     .compileComponents();
   });
 
+  
+  
   beforeEach(() => {
     fixture = TestBed.createComponent(ProblemeComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
-  });
+  }); 
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  
+  it('champ nom du probleme doit comporter au moins 3 caracteres', () => {
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue('a'.repeat(3));
+    expect(zone.valid).toBeTruthy();
   });
-  it('champ nom du produit doit comporter au moins 3 caracteres', () => {
-    expect(true).toBeTruthy();
-  });
+  it("#1 | Zone PRÉNOM invalide avec 2 caractèress", () =>{
+    let zone = component.problemeForm.controls['prenom'];
+    zone.setValue("a".repeat(2));
+    let errors = zone.errors || {};
+    expect(errors['minlength']).toBeTruthy();
+    });
 
-});
+    it("#1 | Zone PRÉNOM valide avec 3 caractèress", () =>{
+      let zone = component.problemeForm.controls['prenom'];
+      zone.setValue("a".repeat(3));
+      let errors = zone.errors || {};
+      expect(errors['minlength']).toBeFalsy();
+      });
+
+      it("#1 | Zone PRÉNOM valide avec 200 caractèress", () =>{
+        let zone = component.problemeForm.controls['prenom'];
+        zone.setValue("a".repeat(200));
+        let errors = zone.errors || {};
+        expect(errors['minlength']).toBeFalsy();
+        });
+  
+  
+  
+      });
--- src/app/probleme/probleme.component.ts
+++ src/app/probleme/probleme.component.ts
@@ -1,16 +1,42 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-probleme',
   templateUrl: './probleme.component.html',
   styleUrls: ['./probleme.component.css']
 })
+
+
+
+
 export class ProblemeComponent implements OnInit {
+  problemeForm: FormGroup;
+  
+  
+  
+  
+  
+  
+  
+  
+  constructor(private fb: FormBuilder) { }
 
-  constructor() { }
-
-  ngOnInit(): void {
+  ngOnInit() {
+    this.problemeForm = this.fb.group({
+        //nomProbleme: ['',[Validators.minLength(3)]],
+        prenom: ['',[Validators.minLength(3)]]
+      });
+  
+  
+  
   }
+  
+  
+  
+  
+  
+  
   save(): void {
   }
 }
